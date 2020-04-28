b7e00ae6fdae01f0e60cf1afeca2fa8718aafbe0
Bryce Harmsen <bryce.harmsen@gmail.com>
2020-04-27 21:46:49 -0700

    train/test/validation data in csv, data preprocess in project1.py

--- lab1/project1.py
+++ lab1/project1.py
@@ -2,16 +2,33 @@
 # 1. create and preprocess the inputs,
 # 2. pass inputs to the Perceptron for learning
 # 3. display to the user the results
-
+import csv
 from perceptron import Perceptron
 
 #create and preprocess inputs
-inputs = []
+def preprocess(fileName):
+    lists = []
+    with open(fileName, 'rb') as csvFile:
+        reader = csv.reader(csvFile, delimiter=',')
+        currList = []
+        for row in reader:
+            if len(row) == 1:
+                target = row[0]
+                currList.append(-1)
+                lists.append((target, currList))
+                currList = []
+            else:
+                currList.extend(list(map(int, row)))
+    return lists
 
+supervisedLists = preprocess('supervisedData.csv')
+inputs = [value[1] for value in supervisedLists]
+targets = [value[0] for value in supervisedLists]
+print inputs, targets
 #pass inputs to the Perceptron
 learningRate = 0.25
 maxIterations = 10
 percepter = Perceptron(inputs, learningRate, maxIterations)
-percepter.train()
+#percepter.train()
 #display user results
 print('Provide output about perceptron learning results')
