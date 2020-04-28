b3a5aea2f122b1c381c1d23c5f08528a4df3c16a
Bryce Harmsen <bryce.harmsen@gmail.com>
2020-04-27 23:52:26 -0700

    first version. probably some bugs.

--- lab1/perceptron.py
+++ lab1/perceptron.py
@@ -1,42 +1,54 @@
 import numpy as np
 
-#good functions to keep in mind:
-# np.dot()
-# np.sign()
-# np.ones()
-
 class Perceptron(object):
-    def __init__(self, inputs, learningRate, maxIterations):
-        self.inputs = inputs
+    def __init__(self, inputs, targets, learningRate, maxIterations):
+        self.inputs = [row + [-1] for row in inputs]
+        self.targets = targets
         self.learningRate = learningRate
         self.maxIterations = maxIterations
 
-    def initialize(self, numInputs):
-        weights = np.random.rand(1,len(self.inputs[0]))
-        print 'weights: ', weights
-        #randomly fill weight matrix
+    def column(self, array, colIdx):
+        return [row[colIdx] for row in array]
+
+    def initialize(self, numInputs, inputDimension):
+        weights = np.random.rand(numInputs, inputDimension)
         return weights
 
     def learn(self, weights, outputs, targets):
-        for weights_i in weights:
-            pass
-            #do weights_i -= learningRate * (output - target) * input
-            #for each row in weights?
-        print 'Do some learning here (adjust weights)'
+        print weights, self.inputs
+        for i in range(0, len(weights)):
+            for j in range(0, len(weights[0])):
+                weights[i][j] -= self.learningRate * (outputs[i] - targets[i]) * self.inputs[i][j]
+        print 'new weights: ', weights
         return weights
 
     def recall(self, weights):
-        outputs = []
-        for inputRow in self.inputs:
-            outputs.extend(list(np.dot(weights, np.transpose(inputRow))))
+        inputs_T = np.transpose(self.inputs)
+        outputs = [np.dot(weights[i], self.column(inputs_T,i)) for i in range(0, len(weights))]
         outputs = map(int, map(np.sign, outputs))
         print 'recall outputs: ', outputs
         return outputs
     
+    def categorize(self, targets):
+        categories = [1, -1]
+        categorizedTarget = {}
+        for i in range(0, len(targets)):
+            if not categorizedTarget.has_key(targets[i]):
+                categorizedTarget[targets[i]] = categories.pop()
+            targets[i] = categorizedTarget.get(targets[i])
+        return targets
+    
     def train(self):
         #runs algorithm
-        weights = self.initialize(len(self.inputs))
-        #recall and learn will loop for some iterations
-        #or until fully learned
-        self.recall(weights)
-        #self.learn(weights, ... more here to figure out)
+        outputs = self.recall(self.initialize(len(self.inputs), len(self.inputs[0])))
+        self.categorize(self.targets)
+        while (self.maxIterations > 0 and np.sum(np.subtract(outputs, self.targets)) != 0):
+            print 'targets: ', self.targets
+            print 'outputs: ', outputs
+            weights = self.initialize(len(self.inputs), len(self.inputs[0]))
+            #recall and learn will loop for some iterations
+            #or until fully learned
+            outputs = self.recall(weights)
+            self.learn(weights, outputs, self.targets)
+            self.maxIterations -= 1
+        return weights, outputs
--- lab1/project1.py
+++ lab1/project1.py
@@ -14,7 +14,6 @@ def preprocess(fileName):
         for row in reader:
             if len(row) == 1:
                 target = row[0]
-                currList.append(-1)
                 lists.append((target, currList))
                 currList = []
             else:
@@ -24,11 +23,11 @@ def preprocess(fileName):
 supervisedLists = preprocess('supervisedData.csv')
 inputs = [value[1] for value in supervisedLists]
 targets = [value[0] for value in supervisedLists]
-print inputs, targets
+
 # 2. pass inputs to the Perceptron
-learningRate = 0.25
-maxIterations = 10
-percepter = Perceptron(inputs, learningRate, maxIterations)
+learningRate = 0.1
+maxIterations = 10000
+percepter = Perceptron(inputs, targets, learningRate, maxIterations)
 percepter.train()
 # 3. display user results
 print('Provide output about perceptron learning results')
